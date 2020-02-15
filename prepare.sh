fa5079362f05d2f11e883d1fd16bf1aee76d6f7c
gigatesseract <neevarp.star@gmail.com>
2020-02-15 17:28:40 +0530

    multiple file support + usage instructions

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,16 @@
-touch $1
-echo -e "data segment
+#!/usr/bin/env bash
+# USAGE: ./prepare filename.asm [one or more filenames]
+n=$# 
+if [ $# -eq 0 ]
+then
+    echo "Please specify at least one file name (ending with .asm)"
+
+else
+for ((i=1;i<=n;i++))
+do
+	touch ${!i}
+echo -e "
+data segment
     a dw ? ; Initialize value of a
     b dw ? ; Initialize value of b
 data ends
@@ -8,18 +19,20 @@ code segment
     assume cs:code, ds:data
 start:
 
+    mov ax, data
+    mov ds, ax ; Initialize Data Segment
 
 
 proc_name proc
 
-
-ret
+    ret
 proc_name endp
 
-
 code ends
-end start" >> $1
+end start
 
-
-
-
+" > ${!i}
+echo ${!i} written
+done	 
+fi
+    
